754fd0e1292b16e54765cdbc74cfe27dd450ae71
Cem Gencer <cem.gencer@me.com>
2013-03-22 17:27:05 +0200

    design evolving

--- frontend/express/public/javascripts/client/dashboard.js
+++ frontend/express/public/javascripts/client/dashboard.js
@@ -1,4 +1,7 @@
 var GryphonDashboard = (function(GryphonDashboard, $, undefined){
 	
+	$(document).ready( function() {
+		$('.sideBar').height( $(document).height() );
+	});
 
 }(window.GryphonDashboard = window.GryphonDashboard || {}, jQuery));
